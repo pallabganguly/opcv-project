725a3cc720b34a17231c125d6e5555af82c9a1d3
pallabganguly <pallabkumarganguly@gmail.com>
2017-11-21 00:33:55 +0530

    new version

--- resizepos.py
+++ resizepos.py
@@ -1,14 +1,14 @@
 import os
 import cv2
 
-ht = 100
+ht = 50
 i = 0
-os.chdir('/home/pallab/Pictures/data')
+os.chdir('/home/pallab/Desktop/files')
 for pic in os.listdir('.'):
     img = cv2.imread(pic, cv2.IMREAD_GRAYSCALE)
-    size = 56, 100
+    size = ht, ht
     img = cv2.resize(img, size)
-    cv2.imwrite('/home/pallab/opcv/pos/'+str(i)+'.jpg', img)
+    cv2.imwrite('/home/pallab/opcv/pos'+str(ht)+'/p'+str(i)+'.jpg', img)
     i += 1
 
 print('Resized', i, 'files', sep = ' ')
--- sample.py
+++ sample.py
@@ -1,29 +1,25 @@
 import numpy as np
 import cv2
 
-face_cascade = cv2.CascadeClassifier('casc.xml')
-#face_cascade2 = cv2.CascadeClassifier('/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('./haarcascade_eye.xml')
 hand_cascade = cv2.CascadeClassifier('cascade.xml')
 
-img = cv2.imread('group.jpg')
+img = cv2.imread('groupwa.jpg')
+# img = cv2.resize(img, (900, 900))
 # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 gray = cv2.imread('group.jpg', 0)
 
-faces = face_cascade.detectMultiScale(gray, 1.03, 20)
-hands = hand_cascade.detectMultiScale(gray, 1.03, 20)
-for (x,y,w,h) in faces:
-#print((x,y,w,h))
+hands = hand_cascade.detectMultiScale(gray, 1.32, 24)
+'''for (x,y,w,h) in faces:
     cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
     roi_gray = gray[y:y+h, x:x+w]
     roi_color = img[y:y+h, x:x+w]
     eyes = eye_cascade.detectMultiScale(roi_gray)
     for (ex,ey,ew,eh) in eyes:
-        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)'''
         
 for (x,y,w,h) in hands:
 	cv2.rectangle(img,(x,y),(x+w,y+h),(0,31,255),2)
 
-cv2.imshow('image-result',img)
+cv2.imshow('result',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
