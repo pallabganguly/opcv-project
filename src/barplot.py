3664089cab717f06d91a5c45f3187736b8bf53c8
pallabganguly <pallabkumarganguly@gmail.com>
2018-01-05 23:26:18 +0530

    Reorganised Project

---
+++ src/barplot.py
@@ -0,0 +1,63 @@
+import numpy as np
+import cv2
+import os
+from matplotlib import pyplot as plt
+
+def intersectingArea(a ,b ,c ,d ,e ,f ,g ,h):
+    if e>=c or a >= g or b >= h or f>=d:
+        return 0
+    
+    topleftx = max(a ,e)
+    toplefty = max(b ,f)
+    bottomrightx = min(c ,g)
+    bottomrighty = min(d ,h)
+    area = (bottomrightx - topleftx) * (bottomrighty - toplefty)
+    return area
+
+# data to plot
+ssize = len(os.listdir('test50'))
+classifiers = ['cascade_30x30.xml', 'cascade_40x40.xml', 'cascade_50_10.xml', 'cascade_50_15.xml']
+acc = []
+for classifier in classifiers:
+	count = 0
+	for path in os.listdir('test50'):
+		img = cv2.imread('test50/'+path)
+		gray = cv2.imread('test50/'+path, 0)
+		hand_cascade = cv2.CascadeClassifier(classifier)
+		hands = hand_cascade.detectMultiScale(gray, 1.03, 2)
+		for (x,y,w,h) in hands: # (x, y, w, h) predicted values
+			fn = path[:-4]
+			pp = fn.split('_')
+			vals = list(map(int,pp))
+			us, xd, yd, wd, hd = vals # (xd, yd, wd, hd) actual values
+			iarea = intersectingArea(x, y, x+w, y+h, xd, yd, xd+wd, yd+hd)
+			if abs(iarea)/abs(wd*hd) >= 0.75:
+				count += 1
+
+	acc.append((count/ssize)*100)
+	print(classifier, 'Accuracy:',(count/ssize)*100)
+
+n_groups = 4
+means_frank = (0.90, 0.55, 0.75, 65)
+means_guido = (85, 62, 54, 20)
+ 
+# create plot
+fig, ax = plt.subplots()
+index = np.arange(n_groups)
+bar_width = 0.35
+opacity = 0.8
+ 
+rects1 = plt.bar(classifiers, acc, bar_width,
+                 alpha=opacity,
+                 color='c',
+                 )
+ 
+ 
+plt.xlabel('Classifier')
+plt.ylabel('Accuracy %')
+plt.title('Accuracy with different classifiers')
+plt.xticks(index + bar_width, ('30x30', '40x40', '50x50,10', '50x50,15'))
+plt.legend()
+ 
+plt.tight_layout()
+plt.show()
---
+++ src/getaccuracy.py
@@ -0,0 +1,48 @@
+import numpy as np
+import cv2
+import os
+from matplotlib import pyplot as plt
+
+def intersectingArea(a ,b ,c ,d ,e ,f ,g ,h):
+    if e>=c or a >= g or b >= h or f>=d:
+        return 0
+    
+    topleftx = max(a ,e)
+    toplefty = max(b ,f)
+    bottomrightx = min(c ,g)
+    bottomrighty = min(d ,h)
+    area = (bottomrightx - topleftx) * (bottomrighty - toplefty)
+    return area
+
+ssize = len(os.listdir('test50'))
+acc = []
+values = [x for x in range(2, 100)]
+zoom = [1.01+x*0.01 for x in range(0, 100)]
+thres = [0.45+0.02*x for x in range(0, 15)]
+hand_cascade = cv2.CascadeClassifier('cascade_40x40.xml')
+
+for n in thres:
+	count = 0
+	for path in os.listdir('test50'):
+		img = cv2.imread('test50/'+path)
+		gray = cv2.imread('test50/'+path, 0)
+		hands = hand_cascade.detectMultiScale(gray, 1.03, 5)
+		for (x,y,w,h) in hands: # (x, y, w, h) predicted values
+			fn = path[:-4]
+			pp = fn.split('_')
+			vals = list(map(int,pp))
+			us, xd, yd, wd, hd = vals # (xd, yd, wd, hd) actual values
+			iarea = intersectingArea(x, y, x+w, y+h, xd, yd, xd+wd, yd+hd)
+			if abs(iarea)/abs(wd*hd) >= n:
+				count += 1
+	# count = min(count, ssize)
+	# acc.append((count/ssize)*100)
+	# print('n:', n, 'Accuracy:',(count/ssize))
+	acc.append(count)
+	print('n:',n, 'Acc:',count)
+plt.plot(thres, acc)
+# plt.text(4, 8, 'Neigh=5, Scale=1.03', style='italic')
+plt.title('Count of positives vs Threshold')
+plt.xlabel('Threshold')
+plt.ylabel('Counts (Actual=200)')
+plt.show()
