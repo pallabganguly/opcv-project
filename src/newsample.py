35258f54952bb0f0ca666dfe404abb2e61127c37
pallabganguly <pallabkumarganguly@gmail.com>
2018-01-16 18:13:55 +0530

    Side experiment

--- src/newsample.py
+++ src/newsample.py
@@ -3,8 +3,8 @@ import cv2
 
 #face_cascade = cv2.CascadeClassifier('casc.xml')
 #eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-prefix = '/home/pallab/opcv-project/'
-hand_cascade = cv2.CascadeClassifier(prefix+'classifiers/cascade_3050_10.xml')
+prefix = '/home/pallab/opcv-project/classifiers/'
+hand_cascade = cv2.CascadeClassifier(prefix+'cascade_20_15.xml')
 
 cap = cv2.VideoCapture(0)
 
@@ -12,7 +12,8 @@ while True:
 	ret, img = cap.read()
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 	# faces = face_cascade.detectMultiScale(gray, 1.3, 8)
-	hands = hand_cascade.detectMultiScale(gray, 1.8,27, minSize=(25, 25), maxSize=(250, 250))
+	hands = hand_cascade.detectMultiScale(gray, 1.03,25)
+	# hands = hand_cascade.detectMultiScale(gray, 1.8,27, minSize=(25, 25), maxSize=(250, 250))
 	for (x,y,w,h) in hands:
 		# cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
 		cv2.rectangle(img,(x,y),(x+w,y+h),(79,233,252),2)
