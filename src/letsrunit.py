5f58996b108647692e3b7ee7f998f3971f6670c7
pallabganguly <pallabkumarganguly@gmail.com>
2018-01-20 10:56:25 +0530

    added more classifiers

--- src/letsrunit.py
+++ src/letsrunit.py
@@ -9,7 +9,7 @@ img = cv2.resize(img, (900, 600))
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # gray = cv2.imread(prefix + 'group.jpg', 0)
 
-hands = hand_cascade.detectMultiScale(gray, 1.05, 9, minSize=(20, 20),maxSize=(40, 40))# minSize=(50, 50),maxSize=(80, 80))
+hands = hand_cascade.detectMultiScale(gray, 1.05, 9, minSize=(20, 20),maxSize=(35, 35))# minSize=(50, 50),maxSize=(80, 80))
 
 for (x,y,w,h) in hands:
 	cv2.rectangle(img,(x,y),(x+w,y+h),(200,30,200),2)
