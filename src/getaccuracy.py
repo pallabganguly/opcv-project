d178cc504730762aa44dca6aa93c3305615189cb
pallabganguly <pallabkumarganguly@gmail.com>
2018-01-31 16:55:36 +0530

    adjust values again

--- src/getaccuracy.py
+++ src/getaccuracy.py
@@ -16,19 +16,20 @@ def intersectingArea(a ,b ,c ,d ,e ,f ,g ,h):
 
 prefix = '/home/pallab/opcv-project/'
 ssize = len(os.listdir(prefix+'small-test'))
-print(ssize)
+print('Test-set size: ', ssize)
 acc = []
 values = [x for x in range(2, 100)]
-zoom = [1.01+x*0.01 for x in range(0, 100)]
+# zoom = [1.01+x*0.01 for x in range(0, 100)]
+zoom = [1.02+x*0.02 for x in range(0, 200)]
 thres = [0.45+0.02*x for x in range(0, 15)]
 hand_cascade = cv2.CascadeClassifier(prefix+'classifiers/cascade_20_20.xml')
-print(hand_cascade.load(prefix+'classifiers/cascade_40x40.xml'))
+print('Cascade Loaded: ', hand_cascade.load(prefix+'classifiers/cascade_40x40.xml'))
 
 for n in zoom:
 	count = 0
 	for path in os.listdir(prefix+'small-test'):
 		img = cv2.imread(prefix+'small-test/'+path, 0)
-		hands = hand_cascade.detectMultiScale(img, n, 5)
+		hands = hand_cascade.detectMultiScale(img, n, 3)
 		for (x,y,w,h) in hands: # (x, y, w, h) predicted values
 			fn = path[:-4]
 			pp = fn.split('_')
