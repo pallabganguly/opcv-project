cffa4952bf85a7c5c7c65c76e7a24975d109bcbd
Pallab Ganguly <pallabkumarganguly@gmail.com>
2018-04-29 12:27:22 +0530

    for gesture testing

--- src/newsample.py
+++ src/newsample.py
@@ -4,25 +4,18 @@ import cv2
 #face_cascade = cv2.CascadeClassifier('casc.xml')
 #eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 prefix = '/home/pallab/opcv-project/classifiers/'
-hand_cascade = cv2.CascadeClassifier(prefix+'cascade_20_20.xml')
+hand_cascade = cv2.CascadeClassifier(prefix+'cascade_20_15.xml')
 
 cap = cv2.VideoCapture(0)
 
 while True:
 	ret, img = cap.read()
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	# faces = face_cascade.detectMultiScale(gray, 1.3, 8)
-	# hands = hand_cascade.detectMultiScale(gray, 1.03,15)
-	hands = hand_cascade.detectMultiScale(gray, 1.05, 3, minSize=(50, 50),maxSize=(80, 80))
-	for (x,y,w,h) in hands:
-		# cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+	hands = hand_cascade.detectMultiScale(gray, 1.05, 7, minSize=(50, 50),maxSize=(80, 80))
+	for (x, y, w, h) in hands:
 		cv2.rectangle(img,(x,y),(x+w,y+h),(79,233,252),2)
 		roi_gray = gray[y:y+h, x:x+w]
-		roi_color = img[y:y+h, x:x+w]
-		# eyes = eye_cascade.detectMultiScale(roi_gray)
-		hands = hand_cascade.detectMultiScale(roi_gray)
-		# for (ex,ey,ew,eh) in eyes:
-		# 	cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+		roi_color = roi_color = img[y:y+h, x:x+w]
 
 	cv2.imshow('img', img)
 	k = cv2.waitKey(30) & 0xff
